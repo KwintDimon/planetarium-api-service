5c3038e835de5c2c230315bfeba3b0903646ea00
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-07-30 00:05:17 +0300

    created views for ShowThemes, AstronomyShow, ShowSession

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,21 +1,121 @@
-from rest_framework import views, mixins
-from rest_framework.viewsets import GenericViewSet
+from datetime import datetime
+
+from django.db.models import F, Count
+
+from rest_framework import viewsets, mixins
+
 
 from .permissions import IsAdminOrIfAuthenticatedReadOnly
 
 from .models import (
     PlanetariumDome,
+    ShowTheme,
+    AstronomyShow,
+    ShowSession,
 )
 from .serializers import (
-    PlanetariumDomeSerializer
+    PlanetariumDomeSerializer,
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+    AstronomyShowListSerializer,
+    AstronomyShowDetailSerializer,
+    ShowSessionSerializer,
 )
 
 
 class PlanetariumDomeViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
-    GenericViewSet
+    viewsets.GenericViewSet
 ):
     queryset = PlanetariumDome.objects.all()
     serializer_class = PlanetariumDomeSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class ShowThemeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class AstronomyShowViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = AstronomyShow.objects.prefetch_related("themes")
+    serializer_class = AstronomyShowSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    @staticmethod
+    def _params_to_int(qs):
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        themes = self.request.query_params.get("themes")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(
+                title__icontains=title
+            )
+
+        if themes:
+            themes_id = self._params_to_int(themes)
+            queryset = queryset.filter(
+                themes__id__in=themes_id
+            )
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = (
+        ShowSession.objects.all()
+        .select_related(
+            "planetarium_dome", "astronomy_show"
+        )
+        .annotate(
+            tickets_available=(
+                F("planetarium_dome__rows")
+                * F("planetarium_dome__seats_in_row")
+                - Count("tickets")
+            )
+        )
+    )
+    serializer_class = ShowSessionSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        show_id_str = self.request.query_params.get("astronomy_show")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(
+                show_time__date=date
+            )
+
+        if show_id_str:
+            queryset = queryset.filter(
+                astronomy_show_id=int(show_id_str)
+            )
+
+        return queryset
